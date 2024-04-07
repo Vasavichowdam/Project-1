09bba03f663a8061db236c680dcbe4e66f2b5bd4
Vasavichowdam <156642437+Vasavichowdam@users.noreply.github.com>
2024-04-07 19:39:36 +1200

    CartDao.java

---
+++ CatrDao.java
@@ -0,0 +1,98 @@
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.jsp.Shopping_Cart.dto.Cart;
+
+@Repository
+public class CartDao 
+{
+	@Autowired
+    EntityManagerFactory emf;
+	
+	public void saveCart(Cart cart)
+	{
+		EntityManager em=emf.createEntityManager();
+	    EntityTransaction et = em.getTransaction();
+	    
+	    et.begin();
+	    em.persist(cart);
+	    et.commit();
+	    
+	 }
+	
+	public void updateCart(Cart c)
+	{
+		EntityManager em=emf.createEntityManager();
+	    EntityTransaction et = em.getTransaction();
+	    
+	    et.begin();
+	    em.merge(c);
+	    et.commit();
+	}
+	
+	public void deleteCartById(int id)
+	{
+		EntityManager em=emf.createEntityManager();
+	    EntityTransaction et = em.getTransaction();
+	    
+	    Cart c = em.find(Cart.class, id);
+	    
+	    et.begin();
+	    em.remove(c);
+	    et.commit();
+	}
+	
+	public Cart findCartById( int id)
+	{
+		EntityManager em=emf.createEntityManager();
+	    EntityTransaction et = em.getTransaction();
+	    
+	    Cart c = em.find(Cart.class, id);
+	    
+	    if(c!=null)
+		{
+			return c;
+		}
+		else
+		{
+			return null;
+		}
+	    
+	 }
+	
+	public Cart removeItemFromCartById(int cart_id, int item_id)
+	{
+		EntityManager em=emf.createEntityManager();
+	    EntityTransaction et = em.getTransaction();
+	    
+	    Cart c = em.find(Cart.class, item_id);
+	    
+	    et.begin();
+	    em.remove(c);
+	    et.commit();
+	    
+	    return c;
+	}
+	
+	public Cart removeAllItemsFromCart(int id)
+	{
+		EntityManager em=emf.createEntityManager();
+	    EntityTransaction et = em.getTransaction();
+	    
+	    Cart c = em.find(Cart.class, id);
+	    
+	    c.setItems(null);
+	    c.setTotalprice(0);
+	    
+	    et.begin();
+	    em.merge(c);
+	    et.commit();
+	    
+	    return c;
+	}
+
+}
